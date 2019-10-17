0c96ffe0359e0fca4eadbc1ee741d35d4ac5de27
orchaland <orchaland@gmail.com>
2019-10-17 16:50:07 +0200

    doc

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # RentACar
+
+## Install Node.js
+
+https://nodejs.org/en/download/
+
+Create a project directory
+
+npm init
+
+npm install express
+
+Add 
+
+"start": "node server.js"
+
+to
+
+"scripts" in the fake package.json, like: 
+
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  }
+  
+start the server with: npm start
+
+## Send request to the server
+
+Get a car to be rented: 
+
+curl -X GET -H 'Content-Type: application/json' -i http://localhost:8080/cars/AA11BB
+
+Rent a car with: 
+
+curl -X PUT -H 'Content-Type: application/json' -i 'http://localhost:8080/cars/AA11BB?rent=true' --data '{"debut":"10/10/2020", "fin":"20/10/2020"}'
+
+or use a plugin like RestClient.
+
+Get back a car with:
+
+curl -X PUT -H 'Content-Type: application/json' -i 'http://localhost:8080/cars/AA11BB?rent=false'
+
+
--- server.js
+++ server.js
@@ -1,82 +1,41 @@
-
-//L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.  
-var express = require('express'); 
-// Nous définissons ici les paramètres du serveur.
+var express = require('express');
 var hostname = 'localhost'; 
 var port = 8080;
 
-// Nous créons un objet de type Express. 
 var app = express(); 
-
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
+var car = {
+    "brand":"Ferrari",
+    "model":"458 Italia",
+    "plateNumber":"AA11BB",
+    "price":2000
 }
 
+var myRouter = express.Router();
 
-
-
-//Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
-//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes. 
-var myRouter = express.Router(); 
-
-// Je vous rappelle notre route (/piscines).  
-myRouter.route('/cars')
-// J'implémente les méthodes GET, PUT, UPDATE et DELETE
-// GET
-.get(function(req,res){ 
-	  res.json({message : "Liste toutes les piscines de Lille Métropole", methode : req.method, data: user});
-})
-//POST
-.post(function(req,res){
-      res.json({message : "Ajoute une nouvelle piscine à la liste",
-       nom : req.body.nom,
-       ville : req.body.ville,
-       taille : req.body.taille,
-       methode : req.method});
-      })
-//PUT
-.put(function(req,res){ 
-      res.json({message : "Mise à jour des informations d'une piscine dans la liste", methode : req.method});
-})
-//DELETE
-.delete(function(req,res){ 
-res.json({message : "Suppression d'une piscine dans la liste", methode : req.method});  
-}); 
-
-
-
-
-myRouter.route('/cars/:piscine_id')
+myRouter.route('/cars/:plateNumber')
 .get(function(req,res){
-	  res.json({message : "Vous souhaitez accéder aux informations de la piscine n°" + req.params.piscine_id + " " + req.query.id});
+    if(req.params.plateNumber == "AA11BB"){
+        res.json({message : "Info sur la voiture " + req.params.plateNumber, data: car});
+    } else {
+        res.json({message : "Voiture inconnue"});
+    }
+
 })
 .put(function(req,res){
-	  res.json({message : "Vous souhaitez modifier les informations de la piscine n°" + req.params.piscine_id});
-})
-.delete(function(req,res){
-	  res.json({message : "Vous souhaitez supprimer la piscine n°" + req.params.piscine_id});
+    if(req.query.rent == "true"){
+        res.json({message : "Vous souhaitez louer la voiture : " + req.params.plateNumber, debut : req.body.debut, fin : req.body.fin});
+    } else {
+        res.json({message : "Vous souhaitez ramener la voiture : " + req.params.plateNumber});
+    }
 });
 
-
-
-
-// Nous demandons à l'application d'utiliser notre routeur
 app.use(myRouter);  
 
-// Démarrer le serveur 
 app.listen(port, hostname, function(){
 	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
 });
